ae4167a47b74937d05a92ba9c3e0cb35e08db2ad
Aliaksandr Volkau <58636552+potatofree@users.noreply.github.com>
2022-02-25 17:30:07 +0100

    Dashboard v 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "@progress/kendo-theme-material/dist/all.css";
+import { TileLayout } from "@progress/kendo-react-layout";
+import { useState } from "react";
+import "./App.css";
+
+const WidgetOne = () => <div>Widget one</div>;
+const WidgetTwo = () => <div>Widget two</div>;
+
+const initialPositions = [
+  {
+    col: 1,
+    colSpan: 2,
+    rowSpan: 2,
+  },
+  {
+    col: 3,
+    colSpan: 1,
+    rowSpan: 1,
+  },
+];
+
 
 function App() {
+  const [positions, setPositions] = useState(initialPositions);
+
+  const widgets = [
+   	{
+      header: "There will be the weather cast",
+      body: <WidgetOne />,
+    },
+    {
+      header: "Here should be my todo",
+      body: <WidgetTwo />,
+    },
+  ];
+
+  const handleReposition = e => {
+    setPositions(e.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jobs dashboard</h1>
+      <TileLayout
+        className="tileLayout"
+        columns={4}
+        rowHeight={255}
+        gap={{ rows: 10, columns: 10 }}
+        positions={positions}
+        items={widgets}
+        onReposition={handleReposition}
+      />
     </div>
   );
 }
